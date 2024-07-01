67af08b2ce34d0b497bf4f808f542115051b3b7a
thectogeneral <salemolorundare@gmail.com>
2024-07-01 09:15:21 +0100

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const router = express.Router();
+
+const IPSTACK_API_KEY = process.env.IPSTACK_API_KEY;
 
 router.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name || 'Guest';
@@ -10,16 +16,18 @@ router.get('/api/hello', async (req, res) => {
     const clientIp = testIp || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
     try {
-        const response = await axios.get(`http://ip-api.com/json/${clientIp}`);
+        // Get the location and weather data based on the IP address
+        const response = await axios.get(`http://api.ipstack.com/${clientIp}?access_key=${IPSTACK_API_KEY}&fields=city,temperature`);
         const location = response.data.city || 'Unknown Location';
+        const temperature = response.data.temperature || 'Unknown Temperature';
 
         res.json({
             client_ip: clientIp,
             location: location,
-            greeting: `Hello, ${visitorName}!`
+            greeting: `Hello, ${visitorName}! The temperature is ${temperature} degrees Celsius in ${location}.`
         });
     } catch (error) {
-        res.status(500).send('Error fetching location data');
+        res.status(500).send('Error fetching data');
     }
 });
 
